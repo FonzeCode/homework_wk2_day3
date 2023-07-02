3804ca0430f1bdc9981ead46548bebfc1893f3a1
Luis Echeverria <fonze@Luiss-MacBook-Pro.local>
2023-07-02 15:31:42 -0700

    first commit

---
+++ modules_hw/homework_week2_3.py
@@ -0,0 +1,8 @@
+import modules_hw
+
+area = modules_hw.sqFoot(6,4)
+print(area)
+
+
+circumference = modules_hw.c_of_a_circle(5)
+print(circumference)
---
+++ modules_hw/modules_hw.py
@@ -0,0 +1,12 @@
+def sqFoot(length, width):
+    return length * width
+
+# Area = length * width
+
+
+def c_of_a_circle(radius):
+    pi = 3.14
+    return 2 * pi * radius
+
+# c = circumference
+# circumference of a circle = 2pi * radius
